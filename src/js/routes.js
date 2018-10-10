0c455f7c8710f3e28edc2f7caf3c78531e4cdf57
jonmircha <jonmircha@gmail.com>
2018-10-09 19:43:28 -0500

    Se agrega funcionalidad para eliminar fotos del storage y la BD

---
+++ src/js/routes.js
@@ -0,0 +1,63 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+import './config'
+import { signIn, signOut } from './auth'
+import savePhoto from './save_photo'
+import showPhotos from './show_photos'
+import deletePhoto from './delete_photo'
+
+import tplHome from '../html/home.tpl.html'
+import tplAbout from '../html/about.tpl.html'
+import tplContact from '../html/contact.tpl.html'
+import tplAdmin from '../html/admin.tpl.html'
+import tplAdminAuth from '../html/admin_auth.tpl.html'
+
+const routes = () => {
+  const d = document,
+    main = d.querySelector('.Main')
+
+  d.addEventListener('DOMContentLoaded', e => {
+    main.innerHTML = tplHome
+    showPhotos()
+  })
+
+
+  d.addEventListener('click', e => {
+    if (e.target.matches('a[href="#"]')) {
+      e.preventDefault()
+    }
+
+    if (e.target.matches('#home')) {
+      main.innerHTML = tplHome
+      showPhotos()
+    } else if (e.target.matches('#about')) {
+      main.innerHTML = tplAbout
+    } else if (e.target.matches('#contact')) {
+      main.innerHTML = tplContact
+    } else if (e.target.matches('#admin')) {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          main.innerHTML = tplAdminAuth
+          //d.querySelector('.Admin-name').textContent = user.displayName
+          //d.querySelector('.Admin-avatar').src = user.photoURL
+          savePhoto()
+          showPhotos()
+        } else {
+          main.innerHTML = tplAdmin
+        }
+
+        console.log(user)
+      })
+    } else if (e.target.matches('#login')) {
+      signIn()
+    } else if (e.target.matches('#logout')) {
+      signOut()
+    } else if (e.target.matches('.fa-trash')) {
+      deletePhoto(e.target.dataset.photo, e.target.dataset.id)
+    }
+  })
+}
+
+export default routes
