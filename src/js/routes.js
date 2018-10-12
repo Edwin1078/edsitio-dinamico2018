c4937a095dd6fec1b5e95de842e26e7f80a5aea2
jonmircha <jonmircha@gmail.com>
2018-10-11 19:14:15 -0500

    Evento chanage y carga de imágenes dinámicas

--- src/js/routes.js
+++ src/js/routes.js
@@ -13,6 +13,8 @@ import tplAbout from '../html/about.tpl.html'
 import tplContact from '../html/contact.tpl.html'
 import tplAdmin from '../html/admin.tpl.html'
 import tplAdminAuth from '../html/admin_auth.tpl.html'
+import aboutImg1 from '../img/fifi.jpg'
+import aboutImg2 from '../img/amor-de-ninos.jpg'
 
 const routes = () => {
   const d = document,
@@ -34,14 +36,18 @@ const routes = () => {
       showPhotos()
     } else if (e.target.matches('#about')) {
       main.innerHTML = tplAbout
+      d.querySelector('.About-header').innerHTML = `
+        <img src="${aboutImg1}">
+        <img src="${aboutImg2}">
+      `
     } else if (e.target.matches('#contact')) {
       main.innerHTML = tplContact
     } else if (e.target.matches('#admin')) {
       firebase.auth().onAuthStateChanged(user => {
         if (user) {
           main.innerHTML = tplAdminAuth
-          //d.querySelector('.Admin-name').textContent = user.displayName
-          //d.querySelector('.Admin-avatar').src = user.photoURL
+          d.querySelector('.Admin-name').textContent = user.displayName
+          d.querySelector('.Admin-avatar').src = user.photoURL
           savePhoto()
           showPhotos()
         } else {
@@ -56,6 +62,12 @@ const routes = () => {
       signOut()
     } else if (e.target.matches('.fa-trash')) {
       deletePhoto(e.target.dataset.photo, e.target.dataset.id)
+    }
+  })
+
+  d.addEventListener('change', e => {
+    if (e.target.matches('input[type="file"]')) {
+      d.querySelector('.Form-uploader input[type="text"]').value = e.target.value
     }
   })
 }
